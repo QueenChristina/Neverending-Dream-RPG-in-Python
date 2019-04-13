df096a0d8c21e8652e7e8c1bab942f6984069bae
QueenChristina <31525446+QueenChristina@users.noreply.github.com>
2019-04-12 18:33:39 -0700

    Update and rename 4-10-19 Movement Test.py to 4-11-19 Movement Test.py

--- 4-11-19 Movement Test.py
+++ 4-11-19 Movement Test.py
@@ -1,3 +1,8 @@
+
+#Next, OPTIONAL sprite collision detection with player and blocking
+
+
+##########################################################################
 import pygame, sys, time, os
 from pygame.locals import *
 pygame.init()
@@ -27,7 +32,21 @@ image_list = [imgCalendar, imgTest]
 for u in image_list:
     for i in range (len(u)):
         u[i] = pygame.image.load(u[i])
-        
+
+#run things only once: 
+'''def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+@run_once
+def my_function(foo, bar):
+    return foo+bar
+'''
+
 currentframe = 0
 animationframe = 5
 class Sprite(pygame.sprite.Sprite):
@@ -42,26 +61,69 @@ class Sprite(pygame.sprite.Sprite):
         #self.x and self.y is to be added to self.posx and self.posy for a final coordinate
         self.x = 0
         self.y = 0
+        #self.dx and self.dy is the rate at which object moves
         self.dx = dx
         self.dy = dy
         self.posx = pos[0]
         self.posy = pos[1]
+        #is object finished moving to (x, y)?
+        self.donex = False
+        self.doney = True
+        self.movecounter = 0
+    def moveto(self, newlocations, order):
+        # new locations is a the coordinates as tuple; if you want to move to multiple coordinates in successtion
+        # just use self.moveto(coord) in succession?
+        # rectify the two coord moveto() working at the SAME TIME; add a sort of if done previous...then do this now
+        #also get rid of the list, just make is a single point
+        if self.movecounter == order or self.movecounter == (order + 0.5):
+            print ('moving')
+            if (self.posx + self.x) >= (newlocations[0] - abs(self.dx)) and (self.posx + self.x) <= (newlocations[0] + abs(self.dx)):
+                if self.donex == False:
+                    self.movecounter += 0.5
+                    self.donex = True
+            elif (self.posx + self.x) <= newlocations[0]:
+                self.x += self.dx
+                self.donex = False
+                if self.dx < 0:
+                    self.dx *= -1
+            elif (self.posx + self.x) >= newlocations[0]:
+                self.x += self.dx
+                self.donex = False
+                if self.dx > 0:
+                    self.dx *= -1
+            if (self.posy + self.y) >= (newlocations[1] - abs(self.dy)) and (self.posy + self.y) <= (newlocations[1] + abs(self.dy)):
+                if self.doney == False:
+                    self.movecounter += 0.5
+                    self.doney = True
+            elif (self.posy + self.y) <= newlocations[1]:
+                self.y += self.dy
+                self.doney = False
+                if self.dy < 0:
+                    self.dy *= -1
+            elif (self.posy + self.y) >= newlocations[1]:
+                self.y += self.dy
+                self.doney = False
+                if self.dy > 0:
+                    self.dy *= -1
+            return self.x, self.y
+        else:
+            pass
 #help: https://web.archive.org/web/20121126060528/http://eli.thegreenplace.net/2008/12/13/writing-a-game-in-python-with-pygame-part-i/
     def backforth(self, lowlimx, highlimx, lowlimy, highlimy):
         print (objCalendar.x)
         if self.dx < 0 and self.x < lowlimx:
-                self.dx = -self.dx
+                self.dx *= -1
                 print( 'too low')
         elif self.dx > 0 and self.x > highlimx:
                 self.x = highlimx
-                self.dx = -self.dx
+                self.dx *= -1
                 print('too high')
         if self.dy < 0 and self.y < lowlimy:
-                self.dy = -self.dy
+                self.dy *= -1
                 print( 'too low')
         elif self.dy > 0 and self.y > highlimy:
                 self.y = highlimy
-                self.dy = -self.dy
+                self.dy *= -1
                 print('too high')
         self.move()
     def move(self):
@@ -106,7 +168,11 @@ while True:
 
     DISPLAY.fill((60,70,80))
     #if objTest.done == True:
-    objCalendar.update(objCalendar.backforth(-10, 100, -10, 100))
+    #objCalendar.update(objCalendar.backforth(-10, 100, -10, 100))
+    objCalendar.moveto((500, 300), 0)
+    objCalendar.moveto((300, 80), 1)
+    print (objCalendar.movecounter)
+    objCalendar.update()
     #objTest.animate(imgTest, (DISPLAY_X, DISPLAY_Y), 0, 0, True)
 
     pygame.display.update()
